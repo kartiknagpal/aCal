1e31e0abf1523f999ea146395de2763d11707577
Kartik Nagpal <radsfinest@live.in>
2013-07-01 21:43:55 +0530

    fixing build issues

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/kartik/dev/android/acal/bin/AndroidManifest.xml : \
+/home/kartik/dev/android/acal/AndroidManifest.xml \
---
+++ bin/aCal-debug-unaligned.apk.d
@@ -0,0 +1,5 @@
+/home/kartik/dev/android/acal/bin/aCal-debug-unaligned.apk : \
+/home/kartik/dev/android/acal/bin/aCal.ap_ \
+/home/kartik/dev/android/acal/bin/classes.dex \
+/home/kartik/dev/android/acal/src/com/morphoss/acal/davacal/OldCollectionClass.txt \
+/home/kartik/dev/android/acal/src/com/morphoss/acal/database/OldResourcesModification.txt \
